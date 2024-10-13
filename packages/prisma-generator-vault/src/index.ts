121633d106fafe3735f09f9d57ce803fb9e418e6
Dan Pickett <dan.pickett@launchware.com>
2024-10-12 21:16:33 -0400

    rename lib

---
+++ packages/prisma-generator-vault/src/index.ts
@@ -0,0 +1,17 @@
+export { BaseRepository } from "./prisma-generator-vault/BaseRepository.js"
+import { generatorHandler } from "./prisma-generator-vault/generator/GeneratorHelpers.js"
+import { generate } from "./prisma-generator-vault/generator/PrismaQueryModelOptionsGenerator.js"
+
+export type { AbstractQueryModelOptions } from "./prisma-generator-vault/AbstractQueryModelOptions.js"
+export type { HasManyResult } from "./prisma-generator-vault/HasManyResult.js"
+export type { IdType } from "./prisma-generator-vault/IdType.js"
+export type { PageInfo } from "./prisma-generator-vault/PageInfo.js"
+
+generatorHandler({
+  onManifest: () => ({
+    defaultOutput: "./prisma-vault/",
+    prettyName: "Prisma Vault Repository Generator",
+    requiresGenerators: ["prisma-client-js"],
+  }),
+  onGenerate: generate,
+})
